a945d1837c04b2334cd28005ccd87436023e957c
JalalBelsifar <jalal.belsifar@gmail.com>
2021-01-04 17:44:05 -0600

    added folder structure

---
+++ anime_comment/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AnimeCommentConfig(AppConfig):
+    name = 'anime_comment'
---
+++ anime_post/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AnimePostConfig(AppConfig):
+    name = 'anime_post'
