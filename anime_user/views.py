056a45995d13600c90ca571dc47fb2e96ebf5a39
JalalBelsifar <jalal.belsifar@gmail.com>
2021-01-06 00:20:26 -0600

    profile, username works

--- anime_user/views.py
+++ anime_user/views.py
@@ -9,3 +9,9 @@ def index(request):
 def profile_view(request, username):
     profile_insta = AnimeUser.objects.filter(username=username).first()
     return render(request, "anime_user_detail.html", {"pro": profile_insta})
+
+    # anime_holder = {}
+    # user = AnimeUser.objects.get(username=username)
+    # profile_insta = AnimeUser.objects.filter(username=username).first()
+    # anime_holder['pro'] = profile_insta
+    # return render(request, "anime_user_detail.html", anime_holder)
