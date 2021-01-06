bce609884912e80fa2f17b2cdcea732efad7fca7
JalalBelsifar <jalal.belsifar@gmail.com>
2021-01-05 23:46:50 -0600

    added email to model

--- authentication/forms.py
+++ authentication/forms.py
@@ -4,7 +4,7 @@ from anime_user.models import AnimeUser
 class SignUpForm(forms.ModelForm):
     class Meta:
         model = AnimeUser
-        fields = ['id', 'username', 'password', 'about_me', 'avatar']
+        fields = ['id', 'username', 'password', 'about_me', 'email', 'avatar']
     # username = forms.CharField(max_length=50)
     # password = forms.CharField(widget=forms.PasswordInput)
     # about_me = forms.CharField(max_length=150, widget=forms.Textarea)
