66ebfc68b9eac0623f23132b1a33a3ecd29c2ab8
gordomathurin <mathuringordon@gmail.com>
2021-01-12 20:03:14 -0500

    editing and deleting comment working

--- Config/urls.py
+++ Config/urls.py
@@ -43,8 +43,9 @@ urlpatterns = [
     path("animefeed/", FeedView.as_view(), name="animefeed"),
     path("unfollow/<int:user_id>/", UnFollowView.as_view(), name="unfollow"),
     path("follow/<int:user_id>/", FollowView.as_view(), name="follow"),
+    path("editcomment/<int:comment_id>/", edit_comment_view, name="edit_comment"),
+    path("deletecomment/<int:comment_id>/", delete_comment_view, name="delete_comment")
     # path("post/<int:post_id>/newcomment/", comment_form_view),
-    # path("comment/<int:pk>/delete/", del_comment, name="del_comment"),
     # path("comment/<int:comment_id>/edit/", edit_comment, name="edit_comment"),
 ]
 if settings.DEBUG:
