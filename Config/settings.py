086bafdfdd24e8bd2c0a4f604a4b2a0b624d4217
JalalBelsifar <jalal.belsifar@gmail.com>
2021-01-20 08:10:05 -0600

    signup

--- Config/settings.py
+++ Config/settings.py
@@ -15,9 +15,9 @@ import os
 import sys
 
 # code to hide secret key
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
